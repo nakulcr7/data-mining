77b4f9af15273d297cafe6c8dae5f6a197a06b42
Nakul Camasamudram <nakul.cr7@gmail.com>
2018-01-04 00:24:56 +0530

    Added web scraping code

---
+++ web_mining/README.md
@@ -0,0 +1,16 @@
+# Northeastern University Course Relations
+
+Script that queries Northeastern University's dynamic search [site](https://wl11gp.neu.edu/udcprod8/NEUCLSS.p_disp_dyn_sched) for course data, scrapes the page for courses and their associated pre/co-requisites, and output the result in [DOT](https://graphviz.gitlab.io/_pages/doc/info/lang.html) for Graph Visualisation with [GraphViz](https://graphviz.gitlab.io).
+
+
+## Example Usage
+
+``` bash
+./courses.py --instructor "Derbinsky, Nathaniel L." "Fall 2017 Semester" > derbinsky.gv
+./courses.py --subject CS --level UG "Fall 2017 Semester" > cs_undergrad.gv
+./courses.py --subject CS --level GR "Fall 2017 Semester" > cs_grad.gv
+
+dot -Tpng -oderbinsky.png derbinsky.gv
+dot -Tpng -ocs_undergrad.png cs_undergrad.gv
+dot -Tpng -ocs_grad.png cs_grad.gv
+```
--- web_mining/courses.py
+++ web_mining/courses.py
@@ -16,14 +16,17 @@ PROG_NAME = os.path.splitext(os.path.basename(__file__))[0]
 
 _BASE_URL = "https://wl11gp.neu.edu/udcprod8/"
 
+
 # requests base+endpoint via get/post with provided parameters
 # should NOT be called directly
 def _call(endpoint, method, params):
-    return getattr(requests,method)(urljoin(_BASE_URL, endpoint), data=params)
+    return getattr(requests, method)(urljoin(_BASE_URL, endpoint), data=params)
+
 
 # get an endpoint with provided parameters
 def _get(endpoint, params={}):
     return _call(endpoint, "get", params)
+
 
 # post to an endpoint with provided parameters
 def _post(endpoint, params={}):
@@ -32,10 +35,13 @@ def _post(endpoint, params={}):
 ##############################################################################
 ##############################################################################
 
+
 # parses all the options in a select
 # returns {value:string}
 def _parse_select(select):
-	return {str(option["value"]):str(option.string).strip() for option in select.find_all("option")}
+    return {str(option["value"]): str(option.string).strip() 
+            for option in select.find_all("option")}
+
 
 # an (imperfect) Banner form parser
 # provides a dictionary...
@@ -46,19 +52,19 @@ def _parse_select(select):
 #    key: value (hidden fields and selects)
 # }
 def _parse_form(html):
-    retval = { "params":{} }
+    retval = {"params": {}}
     soup = BeautifulSoup(html, "html.parser")
 
     retval["title"] = str(soup.title.string)
 
-    form = soup.find("div", {"class":"pagebodydiv"}).find("form")
-    
+    form = soup.find("div", {"class": "pagebodydiv"}).find("form")
+
     retval["action"] = str(form["action"])
     retval["method"] = str(form["method"])
 
-    for hidden in form.find_all("input", {"type":"hidden"}):
+    for hidden in form.find_all("input", {"type": "hidden"}):
         retval["params"][str(hidden["name"])] = str(hidden["value"])
-    
+
     for select in form.find_all("select"):
         retval["params"][str(select["name"])] = _parse_select(select)
     return retval
@@ -66,16 +72,18 @@ def _parse_form(html):
 ##############################################################################
 ##############################################################################
 
+
 # parses the term-selection form
 # params include {term_code:term_name}
 def termform():
     return _parse_form(_get("NEUCLSS.p_disp_dyn_sched").text)
 
+
 # given the output of termform(),
 # returns a term code given a term name
 def term_to_code(termform, term):
     terms = termform['params']['STU_TERM_IN']
-    inv_terms = {v:k for k,v in terms.items()}
+    inv_terms = {v: k for k, v in terms.items()}
     if term in inv_terms:
         return inv_terms[term]
     else:
@@ -84,16 +92,18 @@ def term_to_code(termform, term):
 ##############################################################################
 ##############################################################################
 
+
 # parses the empy course-search form
 # for a particular term
 def searchform(termcode):
     return _parse_form(_post("NEUCLSS.p_class_select", {"STU_TERM_IN":termcode}).text)
 
+
 # given the output of searchform(),
 # returns an instructor code given an instructor name
 def instructor_to_code(searchform, instructor):
     instructors = searchform['params']['sel_instr']
-    inv_instructors = {v:k for k,v in instructors.items()}
+    inv_instructors = {v: k for k, v in instructors.items()}
     if instructor in inv_instructors:
         return inv_instructors[instructor]
     else:
@@ -102,8 +112,8 @@ def instructor_to_code(searchform, instructor):
 ##############################################################################
 ##############################################################################
 
+
 # parses the html of a course search, returns...
-# TODO: DOCUMENT RETURN WITH EXAMPLES HERE (must be a tuple)
 def _parse_course_listing(html):
     soup = BeautifulSoup(html, "html.parser")
     course_listing = list()
@@ -121,6 +131,7 @@ def _parse_course_listing(html):
     course_listing.append(tuple(current_listing))
     return(course_listing)
 
+
 # execute a course search requeste
 # returns the parsed result: format of your choice (must be a tuple)
 # (see _parse_course_listing for details)
@@ -128,10 +139,10 @@ def coursesearch(termcode,
                  sel_day=[], sel_subj=["%"], sel_attr=["%"],
                  sel_schd=["%"], sel_camp=["%"], sel_insm=["%"], 
                  sel_ptrm=["%"], sel_levl=["%"], sel_instr=["%"], sel_seat=[],
-                 sel_crn="", sel_crse="", sel_title="", sel_from_cred="", sel_to_cred="",
-                 begin_hh="0", begin_mi="0", begin_ap="a",
+                 sel_crn="", sel_crse="", sel_title="", sel_from_cred="", 
+                 sel_to_cred="", begin_hh="0", begin_mi="0", begin_ap="a",
                  end_hh="0", end_mi="0", end_ap="a"):
-    
+
     # required parameters
     # (or Banner gets unhappy)
     params = [
@@ -170,14 +181,11 @@ def coursesearch(termcode,
         ("end_ap", end_ap),
     ]
 
-    # TODO
-    # 1. Take function parameters and add to params
-    # 2. Submit form with parameters
-    # 3. Call _parse_course_listing to parse, return
     return _parse_course_listing(_post("NEUCLSS.p_class_search", params).text)
 
 ##############################################################################
 ##############################################################################
+
 
 # takes in output of coursesearch
 # and outputs to the console a digraph of related courses
@@ -220,6 +228,7 @@ def print_course_dot(*courseinfo):
 ##############################################################################
 ##############################################################################
 
+
 # outputs usage statement and exits
 def usage():
     print("usage:", PROG_NAME, "(--level)*", "(--instructor)*", "(--subject)*", "[--course]", "<term code>")
@@ -228,27 +237,28 @@ def usage():
     print(" <> must be provided")
     sys.exit(2)
 
+
 # 1. parses command-line parameters
 #    - uses term form to validate term
 #    - uses search form to validate level, instructor, subject
 # 2. performs a course search
 # 3. outputs information in DOT format
 def main(argv):
-    opts,args = getopt.getopt(argv,"",["level=", "instructor=", "subject=", "course="])
+    opts, args = getopt.getopt(argv, "", ["level=", "instructor=", "subject=", "course="])
 
     #####
-    
+
     if len(args) != 1:
         usage()
-        
+
     term = args[0]
     termcode = term_to_code(termform(), term)
     if termcode is None:
         print("ERROR: invalid term")
         usage()
-    
+
     #####
-        
+
     sform = searchform(termcode)
     levels = []
     instructors = []
@@ -262,14 +272,14 @@ def main(argv):
                 usage()
             else:
                 course = opt[1]
-                
+
         elif opt[0] == "--level":
             if opt[1] in sform['params']['sel_levl'].keys():
                 levels.append(opt[1])
             else:
                 print("ERROR: invalid level '{}'".format(opt[1]))
                 usage()
-                
+
         elif opt[0] == "--instructor":
             instructorcode = instructor_to_code(sform, opt[1])
             if instructorcode is not None:
@@ -277,7 +287,7 @@ def main(argv):
             else:
                 print("ERROR: invalid instructor '{}'".format(opt[1]))
                 usage()
-                
+
         elif opt[0] == "--subject":
             if opt[1] in sform['params']['sel_subj'].keys():
                 subjects.append(opt[1])
@@ -287,25 +297,26 @@ def main(argv):
 
     if not len(levels):
         levels = ["%"]
-        
+
     if not len(instructors):
         instructors = ["%"]
-        
+
     if not len(subjects):
         subjects = ["%", "%"]
-        
+
     if course is None:
         course = ""
-    
+
     info = coursesearch(termcode,
-        sel_levl=levels,
-        sel_instr=instructors,
-        sel_subj=subjects,
-        sel_crse=course
-    )
-    # #####
-    
+                        sel_levl=levels,
+                        sel_instr=instructors,
+                        sel_subj=subjects,
+                        sel_crse=course
+                        )
+    ######
+
     print_course_dot(*info)
-    
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
